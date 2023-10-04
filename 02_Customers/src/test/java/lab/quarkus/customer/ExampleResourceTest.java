3b1e5bc3e0d1348388a34e56fcdf612678f5bffc
Luis B <luis.beroiza@hotmail.com>
2023-10-04 14:29:25 +0200

    Elimina Test

--- 02_Customers/src/test/java/lab/quarkus/customer/ExampleResourceTest.java
+++ 02_Customers/src/test/java/lab/quarkus/customer/ExampleResourceTest.java
@@ -11,11 +11,6 @@ public class ExampleResourceTest {
 
   @Test
   public void testHelloEndpoint() {
-    given()
-        .when().get("/hello")
-        .then()
-        .statusCode(200)
-        .body(is("Hello from RESTEasy Reactive"));
   }
 
 }
