4d56e09feb278231e489371763a3110b443798ed
Luis B <luis.beroiza@hotmail.com>
2023-10-05 12:01:19 +0200

    Agrega Datos de Prueba a los Microservicios

--- 01_Products/src/main/resources/import.sql
+++ 01_Products/src/main/resources/import.sql
@@ -4,3 +4,8 @@
 -- insert into myentity (id, field) values(2, 'field-2');
 -- insert into myentity (id, field) values(3, 'field-3');
 -- alter sequence myentity_seq restart with 4;
+
+insert into products (id, name, description) values (1, 'Television Sony', 'Television Sony 48 inch plasma led');
+insert into products (id, name, description) values (2, 'Bicycle Cube', 'Electro-Bicycle Cube 26 inch Wheels');
+insert into products (id, name, description) values (3, 'Ferrari Diablo red', 'Auto Ferrari Diablo Red');
+insert into products (id, name, description) values (4, 'Airplane', 'Jet Airplane');
--- 02_Customers/src/main/resources/import.sql
+++ 02_Customers/src/main/resources/import.sql
@@ -4,3 +4,11 @@
 -- insert into myentity (id, field) values(2, 'field-2');
 -- insert into myentity (id, field) values(3, 'field-3');
 -- alter sequence myentity_seq restart with 4;
+
+
+insert into customers (id, code, accountNumber, name, surname, phone, address) values (1, 'LAB', '987654321', 'Luis Alberto', 'Beroiza Osses', '123546', 'Spring Even Street');
+
+insert into products (id, customer, product) values (1, 1, 1);
+insert into products (id, customer, product) values (2, 1, 2);
+insert into products (id, customer, product) values (3, 1, 3);
+insert into products (id, customer, product) values (4, 1, 4);
