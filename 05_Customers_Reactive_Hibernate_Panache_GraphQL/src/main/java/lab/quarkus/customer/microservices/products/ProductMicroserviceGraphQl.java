e964e2a3fd6cd4b0c6d50aacfdcbf7b809df4ca9
Luis B <luis.beroiza@hotmail.com>
2023-10-12 15:27:09 +0200

    Implementa Llamadas al Microservicio Product usando GraphQl

--- 05_Customers_Reactive_Hibernate_Panache_GraphQL/src/main/java/lab/quarkus/customer/microservices/products/ProductMicroserviceGraphQl.java
+++ 05_Customers_Reactive_Hibernate_Panache_GraphQL/src/main/java/lab/quarkus/customer/microservices/products/ProductMicroserviceGraphQl.java
@@ -5,26 +5,26 @@ import io.smallrye.graphql.client.Response;
 import io.smallrye.graphql.client.core.Document;
 import io.smallrye.graphql.client.dynamic.api.DynamicGraphQLClient;
 import io.smallrye.mutiny.Uni;
-import io.vertx.core.json.JsonArray;
-import io.vertx.mutiny.core.buffer.Buffer;
-import io.vertx.mutiny.ext.web.client.HttpResponse;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
 import jakarta.inject.Named;
 import lab.quarkus.customer.entities.Product;
+import lombok.extern.slf4j.Slf4j;
 import org.eclipse.microprofile.graphql.GraphQLApi;
 
-import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Optional;
 
+import static io.smallrye.graphql.client.core.Argument.arg;
+import static io.smallrye.graphql.client.core.Argument.args;
 import static io.smallrye.graphql.client.core.Field.field;
 import static io.smallrye.graphql.client.core.Operation.operation;
 
-@ApplicationScoped
+@Slf4j
 @GraphQLApi
 @Named("ProductMicroserviceGraphQl")
+@ApplicationScoped
 public class ProductMicroserviceGraphQl implements ProductMicroservice {
 
   @Inject
@@ -34,11 +34,15 @@ public class ProductMicroserviceGraphQl implements ProductMicroservice {
 
   @Override
   public Uni<Map<Long, Product>> getAllProductsAsMap() {
-    return null;
+    return getAllProductsAsList()
+        .onItem()
+        .transform(this::ProductListToMap);
   }
+
 
   @Override
   public Uni<List<Product>> getAllProductsAsList() {
+    log.info("Query to Product Microservice: productList");
     Document query = Document.document(
         operation(
             field("productList",
@@ -56,13 +60,40 @@ public class ProductMicroserviceGraphQl implements ProductMicroservice {
 
   @Override
   public Uni<Product> getProductById(Long id) {
-    return null;
+    log.info("Query to Product Microservice: product with id: {}", id);
+    Document query = Document.document(
+        operation(
+            field("product",
+                args(arg("productId", id)),
+                field("id"),
+                field("name"),
+                field("description")
+            )
+        )
+    );
+    return dynamicGraphQLClient.executeAsync(query)
+        .onItem()
+        .transform(this::responseToProduct);
+  }
+
+  private Product responseToProduct(Response response) {
+    Product product = response.getObject(Product.class, "product");
+    if(product != null){
+      product.setProductId(product.id);
+    }
+    return product;
   }
 
   private List<Product> responseToProductList(Response response) {
     List<Product> productList = response.getList(Product.class, "productList");
     productList.forEach(product -> product.setProductId(product.id));
     return productList;
+  }
+
+  private Map<Long, Product> ProductListToMap(List<Product> productList) {
+    Map<Long, Product> productMap = new HashMap<>();
+    productList.forEach(product -> productMap.put(product.getProductId(), product));
+    return productMap;
   }
 
 }
